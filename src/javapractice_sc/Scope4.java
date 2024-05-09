931c50d0d2b853bc2225c80d1dd071de1a260524
buseonghyeon <113966101+buseonghyeon@users.noreply.github.com>
2024-05-09 20:20:42 +0900

    Create Scope4.java

---
+++ src/javapractice_sc/Scope4.java
@@ -0,0 +1,12 @@
+package javapractice_sc;
+
+public class ScopeDemo4 {
+    static void a(){
+        String title = "coding everybody";
+    }
+    public static void main(String[] args) {
+        a();
+        //System.out.println(title);
+    }
+ 
+}
