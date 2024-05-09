96f495a356ef916f9a12e2ce8bd3ec506de4c625
buseonghyeon <113966101+buseonghyeon@users.noreply.github.com>
2024-05-09 20:45:44 +0900

    Create example3.java

---
+++ src/javapractice_packages/example3.java
@@ -0,0 +1,9 @@
+package javapractice_packages;
+import javapractice_packages.example.*;
+import javapractice_packages.example2.*;
+
+public class D {
+    public static void main(String[] args) {
+        javapractice_packages.example.B b = new javapractice_packages.example.B();
+    }
+}
