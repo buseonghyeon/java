9074d11f70d6d68fd1c625dcd804a386f30e939e
buseonghyeon <113966101+buseonghyeon@users.noreply.github.com>
2024-05-09 21:02:55 +0900

    생활코딩

--- src/javapractice_packages/example3.java
+++ src/javapractice_packages/example3.java
@@ -4,6 +4,6 @@ import javapractice_packages.example2.*;
 
 public class D {
     public static void main(String[] args) {
-        javapractice_packages.example.B b = new javapractice_packages.example.B();
+        javapractice_packages.example.B b = new javapractice_packages.example.B(); 
     }
 }
